1c390f2cb931b44a8aab04847ab34fb358499f85
David Banham <david@banham.id.au>
2013-06-29 00:29:54 +1000

    Different message

--- server.js
+++ server.js
@@ -2,6 +2,6 @@ var http = require('http');
 var port = process.env.PORT || 1337;
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
+  res.end('This is the test2 repo on '+port+'\n');
 }).listen(port, '127.0.0.1');
 console.log('Server running at http://127.0.0.1:'+port+'/');
