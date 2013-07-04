ae96508008ce045d40f8a91a204b7f63cfc729bb
David Banham <david@banham.id.au>
2013-07-04 21:48:34 +1000

    Listen on all interfaces

--- server.js
+++ server.js
@@ -3,5 +3,5 @@ var port = process.env.PORT || 1337;
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('This is the test2 repo on '+port+'\n');
-}).listen(port, '127.0.0.1');
+}).listen(port, '0.0.0.0');
 console.log('Server running at http://127.0.0.1:'+port+'/');
